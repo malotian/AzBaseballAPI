5c7a3f4f816efb9aec2226dd8ae5d684dd1f5381
Harjinder Singh Dhanjal <malotian@gmail.com>
2024-04-26 08:33:02 -0400

    Controllers/HelperAlumniCollegeController.cs

---
+++ Controllers/HelperAlumniCollegeController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CView.BaseballAPI
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelperAlumniCollegeController : ControllerBase
+    {
+        [HttpGet(Name = "AlumniCollege")]
+        public List<object> Get() {
+            FactoryAlumni factoryAlumni = new FactoryAlumni();
+            factoryAlumni.SetCollegeData();
+            return factoryAlumni.College;
+        }
+    }
+}                                                                                    
